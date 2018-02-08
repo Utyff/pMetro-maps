639148a02779b52221b1b217bfb3d7103481e64d
pmetro.git@gmail.com <pmetro.git@gmail.com>
2018-02-08 14:44:18 +0300

    fix pack

--- pack.cmd
+++ pack.cmd
@@ -9,5 +9,7 @@ set MAPS_DIR=maps
 del /qf %ZIP_DIR%\* > nul
 
 FOR /D %%G in ("%MAPS_DIR%\*") DO (
-  %ZIP7% a -tzip -bb0 %ZIP_DIR%\%%~nxG .\%MAPS_DIR%\%%~nxG\* > nul
+  %ZIP7% a -tzip -bb0 %ZIP_DIR%\%%~nxG.pmz .\%MAPS_DIR%\%%~nxG\*  > nul
+  %ZIP7% a -tzip -bb0 %ZIP_DIR%\%%~nxG     .\%ZIP_DIR%\%%~nxG.pmz > nul
+  del %ZIP_DIR%\%%~nxG.pmz
  )
